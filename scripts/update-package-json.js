9900dbf8c40ff4790a96dc66f49a5c36837b8505
Maxime Bourmaud <maxime.bourmaud@spendesk.com>
2023-03-07 10:13:40 +0100

    chore: bump version

--- scripts/update-package-json.js
+++ scripts/update-package-json.js
@@ -9,9 +9,22 @@ const extraScripts = Object.fromEntries(
     ([key]) => !Object.keys(localPackageJson.scripts).includes(key)
   )
 );
+
+// update lint-staged section of local package.json with additional lint-staged from shared package.json
+const extraLintStaged = Object.fromEntries(
+  Object.entries(templatePackageJson["lint-staged"]).filter(
+    ([key]) => !Object.keys(localPackageJson["lint-staged"]).includes(key)
+  )
+);
+
 localPackageJson.scripts = {
   ...localPackageJson.scripts,
   ...extraScripts,
+};
+
+localPackageJson["lint-staged"] = {
+  ...localPackageJson["lint-staged"],
+  ...extraLintStaged,
 };
 
 // update local package.json file
