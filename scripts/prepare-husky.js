1304646214b88a56fa4ae335b481f6eb153d5459
Maxime Bourmaud <maxime.bourmaud@spendesk.com>
2023-03-07 10:04:42 +0100

    feat: simplify husky scripts and add lint-staged to template

--- scripts/prepare-husky.js
+++ scripts/prepare-husky.js
@@ -1,45 +1,15 @@
 const { execSync } = require('child_process');
 const { existsSync } = require('fs');
 
-// ===========
-// File paths.
-// ===========
+const filesCommands = [
+  ['./.husky/_/husky.sh', 'npx husky install'],
+  ['./.husky/pre-commit', 'npx husky add .husky/pre-commit "npx lint-staged"'],
+  ['./.husky/commit-msg', 'npx husky add .husky/commit-msg "npx --no -- commitlint --edit ${1}"']
+ ]
 
-const FILE_COMMIT = './.husky/pre-commit';
-const FILE_COMMIT_MESSAGE = './.husky/commit-msg';
-const FILE_HUSKY = './.husky/_/husky.sh';
-
-// =========
-// Commands.
-// =========
-
-const CLI_COMMIT = 'npx husky add .husky/pre-commit "npx lint-staged"';
-const CLI_COMMIT_MESSAGE = 'npx husky add .husky/commit-msg "npx --no -- commitlint --edit ${1}"';
-const CLI_HUSKY = 'npx husky install';
-
-// ==============
-// Husky install.
-// ==============
-
-if (!existsSync(FILE_HUSKY)) {
-  global.console.log(CLI_HUSKY);
-  execSync(CLI_HUSKY);
-}
-
-// ====================
-// Add commit-msg hook.
-// ====================
-
-if (!existsSync(FILE_COMMIT_MESSAGE)) {
-    global.console.log(CLI_COMMIT_MESSAGE);
-    execSync(CLI_COMMIT_MESSAGE);
+ filesCommands.map(fileCommand => {
+  if (!existsSync(fileCommand[0])) {
+    global.console.log(fileCommand[1]);
+    execSync(fileCommand[1]);
   }
-
-// ====================
-// Add pre-commit hook.
-// ====================
-
-if (!existsSync(FILE_COMMIT)) {
-    global.console.log(CLI_COMMIT);
-    execSync(CLI_COMMIT);
-  }
+ });
